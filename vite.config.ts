cdce1b58bf26f2175406fb24f2eec490747ae512
NelsonJoshua03 <joshwords2020@gmal.com>
2025-09-22 22:05:28 +0530

    Initial commit of resume builder website with TypeScript fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://resumecvforge.netlify.app',
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
@@ -12,4 +18,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['pdfjs-dist'],
   },
+  build: {
+    sourcemap: true,
+    // Ensure public files are copied
+    copyPublicDir: true,
+  },
+  publicDir: 'public',
 })
